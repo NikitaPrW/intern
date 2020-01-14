2cfb61f62df5f4d3bc7c290fef0046e25a53e1d7
NikitaPrW <privalov.nikita@wildberries.ru>
2020-01-14 20:08:03 +0300

    test for function

--- first_task/TwoIntSum_test.go
+++ first_task/TwoIntSum_test.go
@@ -1,25 +1,33 @@
-package main
+package sum
 
 import (
 	"fmt"
+	"log"
+	"testing"
 )
 
-//for a[i] range Slice
+func TWoIntSumTest(t *testing.T) {
+	log.SetFlags(log.Ltime | log.Lshortfile)
+	var result []int
+	testtargets := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	testarrays := [][]int{
+		[]int{1, 2, 3, 4, 5, 6, 7, 8, 9},
+		[]int{1, 3, 5, 1, 2, 5, 6, 7, 234, 2, 6, 1, 4, 6, 7, 5},
+	}
+	var failsCount int
 
-func twoIntSum(nums []int, target int) []int {
-	var solution []int
-	for i, value := range nums {
-		for n := i + 1; n < len(nums); n++ {
-			if value+nums[n] == target {
-				solution = append(solution, i, n)
-				return solution
+	for _, test := range testarrays {
+		for index := 0; index < len(testtargets); index++ {
+			result = TwoIntSum(test, testtargets[index])
+
+			if len(result) != 2 {
+				failsCount++
+				log.Println("couldn't find numbers", test, testtargets[index])
 			}
 		}
 	}
-	fmt.Println(target)
-}
-
-func main() {
-	a := []int{0, 1, 2, 3, 4}
-	twoIntSum(a, 5)
+	if failsCount == len(testtargets) {
+		fmt.Println("all test failed")
+		t.Fail()
+	}
 }
