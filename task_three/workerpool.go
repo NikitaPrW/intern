bec471f1cbe3143abf778eebaed1a898da583485
NikitaPrW <privalov.nikita@wildberries.ru>
2020-01-16 11:06:27 +0300

    task three

--- task_three/workerpool.go
+++ task_three/workerpool.go
@@ -12,16 +12,22 @@ type task struct {
 }
 
 func workerpool(numOfWorkers int) map[int]int {
+	//var wg sync.WaitGroup
+	//wg.Add(numOfWorkers)
 	work := make(map[int]int)
 	goroutineresult := make(chan task)
+	//goroutineresult := make(chan task, numOfWorkers)
 	for i := 0; i < numOfWorkers; i++ {
-		a := time.Duration(rand.Intn(2) + 1)
-		go func(timetoSleep time.Duration) {
-			goroutineresult <- task{i, i + i}
-			time.Sleep(a * time.Second)
-		}(a)
+		rand.Seed(time.Now().UnixNano())
+		a := time.Duration(rand.Intn(3) + 1)
+		go func(timetoSleep time.Duration, index int) { //создается го рутина
+			time.Sleep(timetoSleep * time.Second)
+			goroutineresult <- task{index, index + index}
+			//wg.Done()
+		}(a, i) //в го рутину передаются a,i, чтобы в процессе работы го рутины не поменялось их значение для го рутины
 	}
-	for i := 0; i < numOfWorkers; i++ {
+	//wg.Wait()
+	for i := 0; i < numOfWorkers; i++ { //запись из канала в map
 		task := <-goroutineresult
 		work[task.id] = task.result
 	}
@@ -30,6 +36,7 @@ func workerpool(numOfWorkers int) map[int]int {
 			fmt.Println("Incorrect result", work[i], i+i)
 		}
 	}
-	fmt.Println("////", work)
+	fmt.Println("////////////////////////////////////////////////")
+	fmt.Println(work)
 	return work
 }
--- task_three/workerpool_benchmark_test.go
+++ task_three/workerpool_benchmark_test.go
@@ -2,20 +2,23 @@ package workerpool
 
 import (
 	"log"
-	"math/rand"
 	"os"
 	"testing"
 )
 
 func BenchmarkWorkerpool(b *testing.B) {
 	var iscorrect bool = true
-	numofworkers := rand.Intn(10)
+	//numofworkers := rand.Intn(10)
+	numofworkers := 10
 	result := make(map[int]int)
+	b.ResetTimer()
+	b.StartTimer()
 	b.Run("Run the task", func(b *testing.B) {
 		result = workerpool(numofworkers)
 	})
+	b.StopTimer()
 	printresult := func(b *testing.B, iscorrect bool) {
-		f, err := os.OpenFile("testlogfile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		f, err := os.OpenFile("testlogfile.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 		if err != nil {
 			log.Fatalf("error opening file: %v", err)
 			log.Println("Counldn't open the file")
