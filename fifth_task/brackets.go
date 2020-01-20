ca1f0a99fe91ce393e1cafbb735a2076a938b7ae
NikitaPrW <privalov.nikita@wildberries.ru>
2020-01-20 13:20:05 +0300

    added bracket checker

--- fifth_task/brackets.go
+++ fifth_task/brackets.go
@@ -1,5 +1,6 @@
 package bracketchecker
 
+// функция проверяет корректность открытых/закрытых скобок
 func isValid(s *string) bool {
 
 	bracketlist := map[rune]rune{
@@ -12,12 +13,12 @@ func isValid(s *string) bool {
 	esi := 0
 	for _, bracket := range *s {
 
-		if bracket == '{' || bracket == '[' || bracket == '(' {
+		if bracket == '{' || bracket == '[' || bracket == '(' { //если встречается открывающая скобка, то она записывается на стэк, и указатель стэка увеличивается на 1
 			stack[esi] = bracketlist[bracket]
 			esi++
 		}
 
-		if bracket == '}' || bracket == ']' || bracket == ')' {
+		if bracket == '}' || bracket == ']' || bracket == ')' { // проверка:если найдена закрывающая скобка, то проверка на то, что предыдущая скобка была того же типа и открывающая, если все ок, то указатель на стэк ументшается
 			if stack[esi-1] == bracket && esi > 0 {
 				esi--
 			} else {
